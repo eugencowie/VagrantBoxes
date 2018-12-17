f6a2c1a5c9ee22f3fdb9026369ac2b57ccf0610b
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-12-17 23:47:53 +0000

    [arch] Modularise sample provision scripts

--- archlinux-minimal/sample/provision/20-locale.sh
+++ archlinux-minimal/sample/provision/20-locale.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 # https://wiki.archlinux.org/index.php/Locale#Generating_locales
-echo en_GB.UTF-8 UTF-8 > /etc/locale.gen && locale-gen >/dev/null
+echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen >/dev/null
 
 # https://wiki.archlinux.org/index.php/Locale#Setting_the_system_locale
 localectl set-locale LANG=en_GB.UTF-8
