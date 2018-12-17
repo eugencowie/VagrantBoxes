35df748f5dd75e6f512674afa8c127b8a43cddbe
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-12-17 22:14:36 +0000

    [arch] Add variable for boot wait time

--- archlinux-minimal/build.sh
+++ archlinux-minimal/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if packer build -only=virtualbox-iso -var mirror=http://archlinux.mirrors.uk2.net archlinux-minimal.json; then
+if packer build -only=virtualbox-iso -var wait=40s -var mirror=http://archlinux.mirrors.uk2.net archlinux-minimal.json; then
    vagrant box add -f archlinux-minimal archlinux-minimal-virtualbox.box
 fi
