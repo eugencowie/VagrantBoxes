058ef5ec9013b2392088c0f6c00d5f29dba9392d
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-12-17 16:00:47 +0000

    [arch] Remove unnecessary boot partition

--- archlinux-minimal/install/install.sh
+++ archlinux-minimal/install/install.sh
@@ -12,17 +12,14 @@ else exit 1; fi
 
 # https://wiki.archlinux.org/index.php/Installation_guide#Partition_the_disks
 parted --script "$device" mklabel msdos
-parted --script "$device" mkpart primary ext2 1MiB 201MiB
-parted --script "$device" mkpart primary ext4 201MiB 100%
+parted --script "$device" mkpart primary ext4 0% 100%
 parted --script "$device" set 1 boot on
 
 # https://wiki.archlinux.org/index.php/Installation_guide#Format_the_partitions
-mkfs.ext2 -q "${device}1"
-mkfs.ext4 -q "${device}2"
+mkfs.ext4 -q "${device}1"
 
 # https://wiki.archlinux.org/index.php/Installation_guide#Mount_the_file_systems
-mount "${device}2" /mnt && mkdir /mnt/boot
-mount "${device}1" /mnt/boot
+mount "${device}1" /mnt
 
 # https://wiki.archlinux.org/index.php/Installation_guide#Select_the_mirrors
 echo "Server = ${mirror}/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
