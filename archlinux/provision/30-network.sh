a10555d94872120511853bceabb153c73b7b4e9e
Eugén Cowie <eugencowie@users.noreply.github.com>
2019-01-09 03:41:04 +0000

    [arch] Separate hostname from network script

--- archlinux/provision/30-network.sh
+++ archlinux/provision/30-network.sh
@@ -4,9 +4,6 @@
 set -e
 set -x
 
-# https://wiki.archlinux.org/index.php/Installation_guide#Network_configuration
-echo archlinux > /etc/hostname
-
 # https://wiki.archlinux.org/index.php/Network_configuration#Revert_to_traditional_interface_names
 ln --symbolic --force /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
