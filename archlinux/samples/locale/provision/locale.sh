1e7c97838ad8c9fe18d4479291dd2ad03cb7cd0f
Eugén Cowie <eugencowie@users.noreply.github.com>
2019-01-11 02:10:56 +0000

    [arch] Simplify sample provision scripts

---
+++ archlinux/samples/locale/provision/locale.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# https://wiki.archlinux.org/index.php/System_time#Time_zone
+timedatectl set-timezone Europe/London
+
+# https://wiki.archlinux.org/index.php/Locale#Generating_locales
+echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen > /dev/null
+localectl set-locale LANG=en_GB.UTF-8
+localectl set-keymap --no-convert uk
+
+# https://wiki.archlinux.org/index.php/Mirrors#Enabling_a_specific_mirror
+echo 'Server = http://archlinux.mirrors.uk2.net/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+pacman --sync --refresh --refresh --sysupgrade --sysupgrade --noconfirm > /dev/null
--- archlinux/samples/swap/provision/swap.sh
+++ archlinux/samples/swap/provision/swap.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-[[ $quiet == true ]] && devnull=/dev/null || devnull=/dev/stdout
 set -e
 set -x
 
 # https://wiki.archlinux.org/index.php/Swap#Swap_file_creation
 fallocate --length 512M /swapfile
 chmod 600 /swapfile
-mkswap /swapfile >$devnull
+mkswap /swapfile > /dev/null
 swapon /swapfile
 echo '/swapfile none swap defaults 0 0' >> /etc/fstab
