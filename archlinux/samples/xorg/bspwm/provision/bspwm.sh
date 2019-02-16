ca3a3e9f773a319da6cde49bd77bc0fcdc520ede
Eugén Cowie <eugencowie@users.noreply.github.com>
2019-02-16 00:50:48 +0000

    [arch] Refactor provision scripts in xorg samples

--- archlinux/samples/xorg/bspwm/provision/bspwm.sh
+++ archlinux/samples/xorg/bspwm/provision/bspwm.sh
@@ -8,13 +8,13 @@ sudo -u vagrant bash -ex << 'END'
 END
 
 # https://wiki.archlinux.org/index.php/Sxhkd#Installation
-pacman --noconfirm --sync sxhkd > /dev/null
+pacman --noconfirm --sync sxhkd rxvt-unicode dmenu ttf-dejavu > /dev/null
 sudo -u vagrant bash -ex << 'END'
   mkdir --parents ~/.config/sxhkd
   cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 END
 
-# https://wiki.archlinux.org/index.php/Xinit#xinitrc
+# https://wiki.archlinux.org/index.php/Bspwm#Starting
 sudo -u vagrant bash -ex << 'END'
   sed --in-place --regexp-extended '/.*(twm|xclock|xterm).*/d' ~/.xinitrc
   echo 'exec bspwm' >> ~/.xinitrc
--- archlinux/samples/xorg/twm/provision/xorg.sh
+++ archlinux/samples/xorg/twm/provision/xorg.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -ex
 
-# https://wiki.archlinux.org/index.php/Xorg#Installation
-pacman --noconfirm --sync xorg-server > /dev/null
-
-# https://wiki.archlinux.org/index.php/Xinit#Autostart_X_at_login
-pacman --noconfirm --sync xorg-xinit > /dev/null
-sudo -u vagrant bash -ex << 'END'
-  cp /etc/X11/xinit/xinitrc ~/.xinitrc
-  echo '[[ ! $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> ~/.bash_profile
-END
-
 # https://wiki.archlinux.org/index.php/VirtualBox#Install_the_Guest_Additions
 pacman --noconfirm --remove virtualbox-guest-utils-nox > /dev/null
 pacman --noconfirm --sync virtualbox-guest-utils > /dev/null
+
+# https://wiki.archlinux.org/index.php/Xorg#Installation
+pacman --noconfirm --sync xorg-server > /dev/null
+
+# https://wiki.archlinux.org/index.php/Xinit#Installation
+pacman --noconfirm --sync xorg-xinit > /dev/null
+sudo -u vagrant bash -ex << 'END'
+  cp /etc/X11/xinit/xinitrc ~/.xinitrc
+END
+
+# https://wiki.archlinux.org/index.php/VirtualBox#Launch_the_VirtualBox_guest_services
 sudo -u vagrant bash -ex << 'END'
   sed --in-place '/.*exec.*/i VBoxClient-all &' ~/.xinitrc
+END
+
+# https://wiki.archlinux.org/index.php/Xinit#Autostart_X_at_login
+sudo -u vagrant bash -ex << 'END'
+  echo '[[ ! $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> ~/.bash_profile
 END
 
 # https://wiki.archlinux.org/index.php/Getty#Automatic_login_to_virtual_console
