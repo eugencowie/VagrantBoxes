ee0622bdcde57343f1e4a1d184d4b76266d91b05
Eugén Cowie <eugencowie@users.noreply.github.com>
2019-05-22 23:33:26 +0100

    [arch] Update version

--- archlinux/build.sh
+++ archlinux/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-packer build -only=virtualbox-iso -var boot=40s -var mirror=http://archlinux.mirrors.uk2.net archlinux.json \
+packer build -only=virtualbox-iso -var boot=40s -var version=2019.05.02 -var mirror=http://archlinux.mirrors.uk2.net archlinux.json \
   && vagrant box add -f archlinux archlinux-virtualbox.box
